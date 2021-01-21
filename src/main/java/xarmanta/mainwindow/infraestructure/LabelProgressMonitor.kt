16818499cc57f2db033d331fa4557d9c975b3701
inigodm <inigo.delgado@gmail.com>
2021-01-21 12:31:03 +0100

    Created a LabelMonitor to monitor changes on blocking screen

---
+++ src/main/java/xarmanta/mainwindow/infraestructure/LabelProgressMonitor.kt
@@ -0,0 +1,58 @@
+package xarmanta.mainwindow.infraestructure
+
+import javafx.application.Platform
+import javafx.scene.control.Label
+import org.eclipse.jgit.lib.BatchingProgressMonitor
+
+class LabelProgressMonitor(val label: Label) : BatchingProgressMonitor() {
+
+    override fun onUpdate(taskName: String, workCurr: Int) {
+        val s = StringBuilder()
+        format(s, taskName, workCurr)
+        send(s)
+    }
+
+    override fun onEndTask(taskName: String, workCurr: Int) {
+        val s = StringBuilder()
+        format(s, taskName, workCurr)
+        s.append("\n")
+        send(s)
+    }
+
+    private fun format(s: StringBuilder, taskName: String, workCurr: Int) {
+        s.append("\r$taskName: ")
+        while (s.length < 25) s.append(' ')
+        s.append(workCurr)
+    }
+
+    override fun onUpdate(taskName: String, cmp: Int, totalWork: Int, pcnt: Int) {
+        val s = StringBuilder()
+        format(s, taskName, cmp, totalWork, pcnt)
+        send(s)
+    }
+
+    /** {@inheritDoc}  */
+    override fun onEndTask(taskName: String, cmp: Int, totalWork: Int, pcnt: Int) {
+        val s = StringBuilder()
+        format(s, taskName, cmp, totalWork, pcnt)
+        s.append("\n")
+        send(s)
+    }
+
+    private fun format(s: StringBuilder, taskName: String, cmp: Int, totalWork: Int, pcnt: Int) {
+        s.append("\r$taskName: ")
+        while (s.length < 25) s.append(' ')
+        val endStr = totalWork.toString()
+        var curStr = cmp.toString()
+        while (curStr.length < endStr.length) curStr = " $curStr" //$NON-NLS-1$
+        if (pcnt < 100) s.append(' ')
+        if (pcnt < 10) s.append(' ')
+        s.append("$pcnt% ($curStr/$endStr)")
+    }
+
+    private fun send(s: StringBuilder) {
+        Platform.runLater{
+            label.text = s.toString()
+        }
+    }
+}
